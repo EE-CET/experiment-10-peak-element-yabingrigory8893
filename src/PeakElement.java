dbc894182bd0af92624ca67f0c040630dd46b9e1
yabingrigory8893 <yabingrigory3@gmail.com>
2026-04-09 22:57:05 -0700

    Update PeakElement.java

--- src/PeakElement.java
+++ src/PeakElement.java
@@ -1,7 +1,45 @@
+import java.util.Scanner;
+
 public class PeakElement {
-    
-        // TODO: Read n
-        // TODO: Read the array elements
-        // TODO: Find the first peak element and print its index
-    
+
+    public static void main(String[] args) {
+        try (Scanner sc = new Scanner(System.in)) {
+            if (!sc.hasNextInt()) return;
+            int n = sc.nextInt();
+            int[] arr = new int[n];
+            for (int i = 0; i < n; i++) {
+                if (sc.hasNextInt()) {
+                    arr[i] = sc.nextInt();
+                }
+            }
+            
+            int peakIndex = -1;
+            if (n == 1) {
+                peakIndex = 0;
+            } else {
+                for (int i = 0; i < n; i++) {
+                    if (i == 0) {
+                        if (arr[0] >= arr[1]) {
+                            peakIndex = 0;
+                            break;
+                        }
+                    } else if (i == n - 1) {
+                        if (arr[n - 1] >= arr[n - 2]) {
+                            peakIndex = n - 1;
+                            break;
+                        }
+                    } else {
+                        if (arr[i] >= arr[i - 1] && arr[i] >= arr[i + 1]) {
+                            peakIndex = i;
+                            break;
+                        }
+                    }
+                }
+            }
+            
+            System.out.println(peakIndex);
+        }
+    }
 }
+
+
